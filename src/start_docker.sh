e27aaed70f9e4973c9f2216ea453d508c11c4254
anxu829 <anxu829@gmail.com>
2018-11-12 15:49:29 +0800

    get code for test

--- src/start_docker.sh
+++ src/start_docker.sh
@@ -2,4 +2,7 @@ docker stop el_withlab
 docker rm el_withlab
 nvidia-docker run --name detectron_tutorial -it \
   -v /home:/home \
-  -p 14130:14130 el:withlab /bin/bash -c "cd / && jupyter lab --port 14130 --ip 0.0.0.0 --allow-root"
+  -p 14130:14130 el:withlab /bin/bash -c \
+  "cd / &&  \
+   ln -sfn /home/xuan/work/EL/jingke_vi jk \
+   jupyter lab --port 14130 --ip 0.0.0.0 --allow-root"
