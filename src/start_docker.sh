e4e2cf59ed59c7d0b42e140028a3c8f8d719b9bd
anxu829 <anxu829@gmail.com>
2018-11-12 15:27:02 +0800

    fix bugs

--- src/start_docker.sh
+++ src/start_docker.sh
@@ -2,4 +2,4 @@ docker stop el_withlab
 docker rm el_withlab
 nvidia-docker run --name detectron_tutorial -it \
   -v /home:/home \
-  -p 14130:14130 el_withlab /bin/bash -c "cd / && jupyter lab --port 14130 --ip 0.0.0.0 --allow-root"
+  -p 14130:14130 el:withlab /bin/bash -c "cd / && jupyter lab --port 14130 --ip 0.0.0.0 --allow-root"
